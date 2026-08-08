2b3dd7ea40ae1a01e962e6985aae5418bb70ccf5
lenavalence14 <lenavalence14@gmail.com>
2026-08-08 12:14:33 +0100

    Supprime le dossier dupliqué tuneo.v2-main résiduel

--- tuneo.v2-main/build.gradle.kts
+++
@@ -1,7 +0,0 @@
-// Top-level build file
-plugins {
-    id("com.android.application") version "8.6.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.2.0" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.0" apply false
-}
